bea141fa08ac2cbad4e7087d8c2364a2ce18503d
Pranav Aurangabadkar <ed18s007@smail.iitm.ac.in>
2020-05-21 13:24:11 +0530

    Reverted

--- 01_Data_Stuctures/01_Arrays/02_rotate_array.py
+++
@@ -1,5 +0,0 @@
-#code
-for T in range(int(input())):
-    N, D = map(int, input().split())
-    arr = list(map(int, input().split()))
-    print(*(arr[D:] + arr[:D]), sep=" ")
--- 01_Data_Stuctures/01_Arrays/03_kth_smallest_element.py
+++
@@ -1,7 +0,0 @@
-#code
-for T in range(int(input())):
-    N = int(input())
-    arr = [map(int, input().split())]
-    k = int(input())
-    arr.sort()
-    print(arr[k-1])
